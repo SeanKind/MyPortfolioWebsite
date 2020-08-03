0748bba1d81e9421ea53692a1d808c7001ab4dc6
Sean Coleman <seancoleman13@gmail.com>
2020-08-02 23:09:26 -0600

    Remove all to start build

--- Portfolio/Portfolio/Controllers/HomeController.cs
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -14,17 +14,9 @@ namespace Portfolio.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = "Sean's Portfolio";
             return View();
         }
     }
